b16d2830c701d921fa573cb5a041296515d3386e
Aryanchaudhari <80584238+Aryanchaudhari@users.noreply.github.com>
2021-04-03 19:57:18 +0530

    Add files via upload

---
+++ C21/collision--main/sketch.js
@@ -0,0 +1,60 @@
+
+var fixedRect, movingRect;
+var gameObject1, gameObject2;
+var wall,car;
+
+function setup() {
+  createCanvas(1200,800);
+ 
+ car=createSprite(100,200,50,50);
+ car.shapeColor="blue";
+ car.velocityX=5;
+
+ wall=createSprite(900,200,10,100);
+ wall.shapeColor="brown"
+ 
+
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  fixedRect = createSprite(100, 100, 50, 50);
+  fixedRect.shapeColor = "green";
+  
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching()){
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
+  }
+  
+  else {
+    movingRect.shapeColor = "green";
+  
+    fixedRect.shapeColor = "green"
+  }
+ 
+
+//bounceOff(movingRect,fixedRect)
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+    
+    return true;
+  }
+  else {
+    return false;
+  } 
+}
+
+
